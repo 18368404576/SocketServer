b6475d2da7e434d6bfd04da1071f1841e4ff0fa5
笑 <jiangzengbing@outlook.com>
2018-05-15 11:12:01 +0800

    添加IP,端口号提示

--- SocketServer/SocketServer/FrmServer.Designer.cs
+++ SocketServer/SocketServer/FrmServer.Designer.cs
@@ -39,26 +39,28 @@
             this.btn_Select = new System.Windows.Forms.Button();
             this.btn_SendFile = new System.Windows.Forms.Button();
             this.btn_Send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_IP
             // 
-            this.txt_IP.Location = new System.Drawing.Point(26, 24);
+            this.txt_IP.Location = new System.Drawing.Point(59, 21);
             this.txt_IP.Name = "txt_IP";
-            this.txt_IP.Size = new System.Drawing.Size(118, 21);
+            this.txt_IP.Size = new System.Drawing.Size(203, 21);
             this.txt_IP.TabIndex = 0;
             // 
             // txt_Log
             // 
-            this.txt_Log.Location = new System.Drawing.Point(26, 64);
+            this.txt_Log.Location = new System.Drawing.Point(26, 61);
             this.txt_Log.Multiline = true;
             this.txt_Log.Name = "txt_Log";
-            this.txt_Log.Size = new System.Drawing.Size(778, 177);
+            this.txt_Log.Size = new System.Drawing.Size(919, 177);
             this.txt_Log.TabIndex = 1;
             // 
             // btn_Start
             // 
-            this.btn_Start.Location = new System.Drawing.Point(344, 22);
+            this.btn_Start.Location = new System.Drawing.Point(555, 19);
             this.btn_Start.Name = "btn_Start";
             this.btn_Start.Size = new System.Drawing.Size(75, 23);
             this.btn_Start.TabIndex = 2;
@@ -68,7 +70,7 @@
             // 
             // btn_StopListen
             // 
-            this.btn_StopListen.Location = new System.Drawing.Point(460, 22);
+            this.btn_StopListen.Location = new System.Drawing.Point(636, 19);
             this.btn_StopListen.Name = "btn_StopListen";
             this.btn_StopListen.Size = new System.Drawing.Size(75, 23);
             this.btn_StopListen.TabIndex = 3;
@@ -79,36 +81,36 @@
             // cmb_Socket
             // 
             this.cmb_Socket.FormattingEnabled = true;
-            this.cmb_Socket.Location = new System.Drawing.Point(576, 25);
+            this.cmb_Socket.Location = new System.Drawing.Point(717, 22);
             this.cmb_Socket.Name = "cmb_Socket";
             this.cmb_Socket.Size = new System.Drawing.Size(228, 20);
             this.cmb_Socket.TabIndex = 4;
             // 
             // txt_Port
             // 
-            this.txt_Port.Location = new System.Drawing.Point(185, 24);
+            this.txt_Port.Location = new System.Drawing.Point(337, 21);
             this.txt_Port.Name = "txt_Port";
-            this.txt_Port.Size = new System.Drawing.Size(118, 21);
+            this.txt_Port.Size = new System.Drawing.Size(203, 21);
             this.txt_Port.TabIndex = 5;
             // 
             // txt_Msg
             // 
-            this.txt_Msg.Location = new System.Drawing.Point(26, 260);
+            this.txt_Msg.Location = new System.Drawing.Point(26, 257);
             this.txt_Msg.Multiline = true;
             this.txt_Msg.Name = "txt_Msg";
-            this.txt_Msg.Size = new System.Drawing.Size(778, 177);
+            this.txt_Msg.Size = new System.Drawing.Size(919, 177);
             this.txt_Msg.TabIndex = 6;
             // 
             // txt_FilePath
             // 
-            this.txt_FilePath.Location = new System.Drawing.Point(26, 456);
+            this.txt_FilePath.Location = new System.Drawing.Point(26, 453);
             this.txt_FilePath.Name = "txt_FilePath";
-            this.txt_FilePath.Size = new System.Drawing.Size(616, 21);
+            this.txt_FilePath.Size = new System.Drawing.Size(757, 21);
             this.txt_FilePath.TabIndex = 7;
             // 
             // btn_Select
             // 
-            this.btn_Select.Location = new System.Drawing.Point(648, 454);
+            this.btn_Select.Location = new System.Drawing.Point(789, 454);
             this.btn_Select.Name = "btn_Select";
             this.btn_Select.Size = new System.Drawing.Size(75, 23);
             this.btn_Select.TabIndex = 8;
@@ -118,7 +120,7 @@
             // 
             // btn_SendFile
             // 
-            this.btn_SendFile.Location = new System.Drawing.Point(729, 454);
+            this.btn_SendFile.Location = new System.Drawing.Point(870, 454);
             this.btn_SendFile.Name = "btn_SendFile";
             this.btn_SendFile.Size = new System.Drawing.Size(75, 23);
             this.btn_SendFile.TabIndex = 9;
@@ -128,7 +130,7 @@
             // 
             // btn_Send
             // 
-            this.btn_Send.Location = new System.Drawing.Point(648, 483);
+            this.btn_Send.Location = new System.Drawing.Point(789, 483);
             this.btn_Send.Name = "btn_Send";
             this.btn_Send.Size = new System.Drawing.Size(75, 23);
             this.btn_Send.TabIndex = 10;
@@ -136,11 +138,31 @@
             this.btn_Send.UseVisualStyleBackColor = true;
             this.btn_Send.Click += new System.EventHandler(this.btn_Send_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(281, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "端口号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "IP：";
+            // 
             // FrmServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(957, 545);
+            this.ClientSize = new System.Drawing.Size(957, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Send);
             this.Controls.Add(this.btn_SendFile);
             this.Controls.Add(this.btn_Select);
@@ -172,6 +194,8 @@
         private System.Windows.Forms.Button btn_Select;
         private System.Windows.Forms.Button btn_SendFile;
         private System.Windows.Forms.Button btn_Send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
